b512acda63e91971824552a2f5130e66830a7a04
weebian <devhxbui@outlook.com>
2024-07-17 21:41:40 -0400

    Update: Instead of relying on an API it now dowloads the torrent files and adds it to a specific folder to download.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # media-server-scripts
-Python script queries a site with Beautiful Soup for videoes to download, which is stored on a Raspberry PI with shared network drives connected through SMB.
+Python script queries a site with Beautiful Soup for videos to download, which is stored on a Raspberry PI/Mini Desktop with shared network drives connected through SMB.
 This project overcomes issues with buffering streams and allows for backup from various devices.
 Updates are sent through smtp.
--- app.py
+++ app.py
@@ -4,20 +4,19 @@ import os
 import random
 import datetime
 import json
-from qbittorrent import Client
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from urllib.request import urlretrieve
 
 #variables
-web_folder_path = "/media/piho/rasp2/webcontent" #folder directory
-anime_folder_path = "/media/piho/rasp2/anime" #anime directory
+web_folder_path = "/media/hoang/rasp2/webcontent" #folder directory
+anime_folder_path = "/media/hoang/rasp2/anime" #anime directory
 A1 = ("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
 ) #Agents
 nurls = ['https://nyaa.si/?f=2&c=1_0&q=&p=1', 'https://nyaa.si/?f=2&c=1_0&q=&p=2', 'https://nyaa.si/?f=2&c=1_0&q=&p=3'] #nyaa url
-surls_ani = ['https://sukebei.nyaa.si/?f=2&c=1_1&q=english'] #sukubei anime
 yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d') #yesterday's date
 
 #Credentials
@@ -44,9 +43,8 @@ def format_message(processed):
         """
         return [text, html]
     
-    animes, sukubei = processed
+    animes = processed
     num_animes = str(len(animes))
-    num_sukubei = str(len(sukubei))
 
     text = f"""\
     Anime downloaded ({num_animes})"""
@@ -62,12 +60,7 @@ def format_message(processed):
         name = anime['anime']
         text += f"\n{name}"
         html += f"<li>{name}</li>"
-
-    text += f"\nSukubei Downloaded ({num_sukubei})"
     html += f"""\
-            </ul>
-            <h1>Sukubei</h1>
-            <p>Downloaded: <b>{num_sukubei}</b></p>
         </body>
         </html>
         """
@@ -123,7 +116,7 @@ def fit_criteria(rows):
 
         #Vars
         name = tds[1].find_all('a', class_=None)[0].contents[0]
-        url = str(tds[2].find_all('a')[1]['href'])
+        url = str(tds[2].find_all('a')[0]['href'])
         date = False
         quality = False
         anime = None
@@ -148,36 +141,6 @@ def fit_criteria(rows):
                 'anime': anime,
                 'url': url
             })
-
-    return results
-
-def fit_criteria_sukebei(rows):
-    #returns a list of torrent links and anime names if matches criterias
-    results = []
-
-    for tr in rows:
-        tds = tr.find_all('td') #obtain tds in the row
-
-        #Checks
-        if len(tds) == 0:
-            continue
-        if not tds[1].find('a'):
-            continue
-        if not tds[2].find('a'):
-            continue
-
-        #var
-        name = tds[1].find('a').contents[0]
-        url = str(tds[2].find_all('a')[1]['href'])
-
-        #Check date
-        if yesterday in str(tds[4].contents[0]):
-            #add to results
-            results.append({
-                'name': name,
-                'url': url
-            })
-        
     return results
 
 def get_anime():
@@ -185,49 +148,29 @@ def get_anime():
 
     #scrape pages and obtain list of tr
     rows = []
-    rows2 = []
 
     for url in nurls: 
         requ = requests.get(url, headers=headers)
         soup = BeautifulSoup(requ.text, 'html.parser')
         rows += soup.find_all('tr')
-        
-    for url in surls_ani:
-        requ2 = requests.get(url, headers=headers)
-        soup2 = BeautifulSoup(requ2.text, 'html.parser')
-        rows2 += soup2.find_all('tr')
 
     #filter
     animes = fit_criteria(rows)
-    s_animes = fit_criteria_sukebei(rows2)
     
-    return [animes, s_animes]
+    return animes
 
-def download(results):
+def download(animes):
     try:
-        #download on qbittorent
-        qb = Client('http://127.0.0.1:8080/')
-        qb.login('admin', 'adminadmin')
-
-        animes = results[0]
-        s_animes = results[1]
-
+        #download files
         for anime in animes:
-            qb.download_from_link(anime.get('url'), category='anime')
-        for s_anime in s_animes:
-            qb.download_from_link(s_anime.get('url'), savepath='/media/piho/rasp2/sukubei/anime', category='sukubei_anime')
+            urlretrieve("https://nyaa.si/" + anime.get('url'), "/home/hoang/deluge/downloads/" + anime.get('anime')+".torrent")
 
         #save results as a log
         with open(web_folder_path + '/anime_' + yesterday + '.txt', 'w') as fout:
             json.dump(animes, fout)
         with open(web_folder_path + '/anime_' + yesterday + '.txt', 'a') as fout:
             fout.write("\nAdded " + str(len(animes)) + " animes")
-        with open(web_folder_path + '/s_animes_' + yesterday + '.txt', 'w') as fout:
-            json.dump(s_animes, fout)
-        with open(web_folder_path + '/s_animes_' + yesterday + '.txt', 'a') as fout:
-            fout.write("\nAdded " + str(len(s_animes)) + " sukubei animes")
         print('Success downloaded ' + str(len(animes)) + ' animes')
-        print('Success downloaded ' + str(len(s_animes)) + ' sukubei animes')
         processed = results
     except:
         processed = None
